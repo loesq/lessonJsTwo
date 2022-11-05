e96405e3260e82634902334a29e9ed6fd99eafeb
loes <loesqq20@mail.ru>
2022-11-05 14:52:16 +0300

    тернарный оператор

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-let age = '4324';
-age = +age
-if (!isNaN(age)) {
-    if (age>18) {
-        console.log('вам больше 18');
-    }else if(age < 18) {
-        console.log('тебе меньше 18');
-    }else if(age===18){
-        console.log('18');
-    }else if(age>150) {
-        console.log('okay');
-    }
-}else {
-    console.log('введите корректные данные');
-};
-
-
-const gender = 'muj'
+const isAdmin = true;
+console.log(`hi, ${
+    isAdmin
+    ? 'admin'
+    : 'user'
+}`);
