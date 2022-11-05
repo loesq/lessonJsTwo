741553cf534028ab127c0db9d47457dfdcac3449
loes <loesqq20@mail.ru>
2022-11-05 13:53:28 +0300

    добавил else

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const age = 15;
-const gender = 'Muj' //
-
-if (gender.toLocaleLowerCase()==='muj') {
-    console.log('тебе налево!');
-}
-if (gender.toLocaleLowerCase()==='jen') {
-    console.log('тебе направо');
+const age = 16;
+const gender = 'muj' //
+if (age>18) {
+    console.log('нормалек');
+}else {
+    console.log('хаха школьник');
 }
