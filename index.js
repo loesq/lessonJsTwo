3a249521c7dcf66073a18f3435f42826fdff7d5b
loes <loesqq20@mail.ru>
2022-11-05 15:18:38 +0300

    switch

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const isAdmin = true;
-console.log(`hi, ${
-    isAdmin
-    ? 'admin'
-    : 'user'
-}`);
+const typeUser = "admin"; // user, guest, bot
+switch (typeUser) {
+  case "admin":
+    console.log("Вы админ");
+    break;
+  case "user":
+    console.log("Вы пользователь");
+    break;
+  case "guest":
+    console.log("Вы гость");
+    break;
+  case "bot":
+    console.log("Вы робот!!");
+    break;
+  default:
+    console.log("who?");
+    break;
+}
