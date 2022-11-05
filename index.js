75ab6b826201cab3f74cb1f7ff0529a32772d268
loes <loesqq20@mail.ru>
2022-11-05 13:48:56 +0300

    добавил if

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+const age = 15;
+const gender = 'Muj' //
+
+if (gender.toLocaleLowerCase()==='muj') {
+    console.log('тебе налево!');
+}
+if (gender.toLocaleLowerCase()==='jen') {
+    console.log('тебе направо');
+}
